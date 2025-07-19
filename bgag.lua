16ca593dca711b8748537226eec5bf8a185a6bba
aliceaia <manhnguyenhp15@gmail.com>
2025-07-19 07:48:53 +0700

    Create bgag.lua

---
+++ bgag.lua
@@ -0,0 +1,3 @@
+repeat wait() until game:IsLoaded() and game.Players.LocalPlayer 
+getgenv().Key = "46dad49a0b7147dfa2c05831" 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BananaHub.lua"))()
