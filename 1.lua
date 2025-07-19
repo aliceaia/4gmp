171edc38a8dc6aeb79239aeb93ee0566423cdf45
aliceaia <manhnguyenhp15@gmail.com>
2025-07-19 19:28:27 +0700

    Update 1.lua

--- 1.lua
+++ 1.lua
@@ -21,7 +21,7 @@ _G.Settings = {
             ["Prickly Pear"] = 1,
             ["Loquat"] = 1,
             ["Feijoa"] = 1,
-            ["Sugar Apple"] = 1
+            ["Sugar Apple"] = 1,
 	},
         Destroy = {
             WhenMoney = 50000,
@@ -33,14 +33,14 @@ _G.Settings = {
                 "Blueberry",
                 "Orange Tulip",
                 "Rose",
-                "Stonebite"
+                "Stonebite",
             }
         }
     },
     SeedsSetting = {
         "Bamboo", "Mushroom", "Cactus", "Coconut", "Dragon Fruit",
         "Cauliflower", "Watermelon", "Rafflesia", "Green Apple", "Avocado", "Banana",
-        "Pineapple", "Kiwi", "Bell Pepper", "Prickly Pear", "Loquat", "Feijoa", "Sugar Apple"
+        "Pineapple", "Kiwi", "Bell Pepper", "Prickly Pear", "Loquat", "Feijoa", "Sugar Apple",
     },
     EggSettings = {
         StartBuyEggWhen = 150000,
